4be978cc8652d89c06f2e7c81b73ee74ceec011a
Daniel Sottile <danielsottile@gmail.com>
2020-09-10 21:03:15 -0400

    added new api calls

--- src/route-all/all-router.js
+++ src/route-all/all-router.js
@@ -1,10 +1,20 @@
-const express = require("express");
-const logger = require("../logger");
-const xss = require("xss");
+const express = require('express');
+const logger = require('../logger');
+const xss = require('xss');
 const AllRouter = express.Router();
-const AllService = require("./all-service");
+const AllService = require('./all-service');
+// temporary for now?
+const BuildService = require('../route-build/build-service');
 
 // Sanitization
+
+const sanitizeFolder = (folder) => ({
+  id: folder.id,
+  folder_name: xss(folder.folder_name),
+  user_id: folder.user_id,
+  date_created: xss(folder.date_created),
+  date_modified: xss(folder.date_modified),
+});
 
 const sanitizeTeam = (team) => ({
   id: team.id,
@@ -56,36 +66,74 @@ const sanitizeSet = (set) => ({
   team_id: set.team_id,
 });
 
-// Teams
+// Folder
 
-AllRouter.route("/search") // Get 10 teams with a search
+AllRouter.route('/folderpublic/:folder_id') // Get a single Folder by ID
   .get((req, res, next) => {
-    const page = Number(req.query.page);
-    const sort = req.query.sort;
-    const species = req.query.species;
-    AllService.getTenTeamsWithSearch(req.app.get("db"), page, sort, species)
-      .then((teams) => {
-        if (!teams) {
-          logger.error("Failed get teams!");
+    const { folder_id } = req.params;
+    BuildService.getSingleUserFolderById(req.app.get('db'), folder_id)
+      .then((folder) => {
+        if (!folder) {
+          logger.error(`Failed get folder with id: ${folder_id}`);
           return res.status(404).json({
-            error: { message: "There Are No Teams" },
+            error: { message: 'folder doesn\'t exist' },
           });
         }
-        logger.info("Successful get 10 teams!");
+        logger.info(
+          `Successful get : folder ${folder.folder_name} was retrieved with id: ${folder.id}`
+        );
+        res.json(sanitizeFolder(folder[0]));
+      })
+      .catch(next);
+  });
+
+// Teams
+
+AllRouter.route('/:folder_id/teams') // Gets the Teams for an individual Folder, used for the public view.
+  .get((req, res, next) => {
+    const { folder_id } = req.params;
+    AllService.getTeamsForIndividualFolder(req.app.get('db'), folder_id)
+      .then((teams) => {
+        if (!teams) {
+          logger.error('Failed get teams for folder!');
+          return res.status(404).json({
+            error: { message: 'There probably isnt this folder' },
+          });
+        }
+        logger.info('Successful get teams for individual folder!');
         res.json(teams.map((team) => sanitizeTeam(team)));
       })
       .catch(next);
   });
 
-AllRouter.route("/:team_id") // Get a team by it's ID
+AllRouter.route('/search') // Get 10 teams with a search
+  .get((req, res, next) => {
+    const page = Number(req.query.page);
+    const sort = req.query.sort;
+    const species = req.query.species;
+    AllService.getTenTeamsWithSearch(req.app.get('db'), page, sort, species)
+      .then((teams) => {
+        if (!teams) {
+          logger.error('Failed get teams!');
+          return res.status(404).json({
+            error: { message: 'There Are No Teams' },
+          });
+        }
+        logger.info('Successful get 10 teams!');
+        res.json(teams.map((team) => sanitizeTeam(team)));
+      })
+      .catch(next);
+  });
+
+AllRouter.route('/:team_id') // Get a team by it's ID
   .get((req, res, next) => {
     const { team_id } = req.params;
-    AllService.getTeamById(req.app.get("db"), team_id)
+    AllService.getTeamById(req.app.get('db'), team_id)
       .then((team) => {
         if (!team[0]) {
           logger.error(`Failed get team with id: ${team_id}`);
           return res.status(404).json({
-            error: { message: "Team doesn't exist" },
+            error: { message: 'Team doesn\'t exist' },
           });
         }
         logger.info(
@@ -98,32 +146,32 @@ AllRouter.route("/:team_id") // Get a team by it's ID
 
 // Sets
 
-AllRouter.route("/:team_id/sets") // Gets the Sets for an individual Team, used for the public view.
+AllRouter.route('/:team_id/sets') // Gets the Sets for an individual Team, used for the public view.
   .get((req, res, next) => {
     const { team_id } = req.params;
-    AllService.getSetsForIndividualTeam(req.app.get("db"), team_id)
+    AllService.getSetsForIndividualTeam(req.app.get('db'), team_id)
       .then((sets) => {
         if (!sets) {
-          logger.error("Failed get sets for teams!");
+          logger.error('Failed get sets for teams!');
           return res.status(404).json({
-            error: { message: "There probably isnt this team" },
+            error: { message: 'There probably isnt this team' },
           });
         }
-        logger.info("Successful get sets for individual team!");
+        logger.info('Successful get sets for individual team!');
         res.json(sets.map((set) => sanitizeSet(set)));
       })
       .catch(next);
   });
 
-AllRouter.route("/set/:set_id") // Get a team by it's ID
+AllRouter.route('/set/:set_id') // Get a team by it's ID
   .get((req, res, next) => {
     const { set_id } = req.params;
-    AllService.getSetById(req.app.get("db"), set_id)
+    AllService.getSetById(req.app.get('db'), set_id)
       .then((set) => {
         if (!set[0]) {
           logger.error(`Failed get set with id: ${set_id}`);
           return res.status(404).json({
-            error: { message: "Set doesn't exist" },
+            error: { message: 'Set doesn\'t exist' },
           });
         }
         logger.info(
