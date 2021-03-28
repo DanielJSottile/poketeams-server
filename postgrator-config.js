339a25d5875673f8702872d800d28eaadf10ef07
Daniel Sottile <danielsottile@gmail.com>
2021-03-28 11:37:11 -0400

    reverted to original config

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,21 +1,10 @@
 require('dotenv').config();
 
 module.exports = {
-  development: {
-    client: 'pg',
-    connection: {
-      connectionString:
-        process.env.NODE_ENV === 'test'
-          ? process.env.DB_TEST_URL
-          : process.env.DATABASE_URL,
-      ssl: !!process.env.SSL,
-    },
-    production: {
-      client: 'postgresql',
-      connection: {
-        connectionString: process.env.DATABASE_URL,
-        ssl: { rejectUnauthorized: false },
-      },
-    },
-  },
+  driver: 'pg',
+  connectionString:
+    process.env.NODE_ENV === 'test'
+      ? process.env.DB_TEST_URL
+      : process.env.DATABASE_URL,
+  ssl: !!process.env.SSL,
 };
