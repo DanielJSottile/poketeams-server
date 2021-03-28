d7fa2bf3cac9792b4184389a9326df2efbfa20f7
Daniel Sottile <danielsottile@gmail.com>
2021-03-27 22:04:52 -0400

    set ssl to true

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 
 module.exports = {
-  'driver': 'pg',
-  'connectionString': process.env.NODE_ENV === 'test' ? process.env.DB_TEST_URL : process.env.DATABASE_URL,
-  'ssl': !!process.env.SSL
+  driver: 'pg',
+  connectionString:
+    process.env.NODE_ENV === 'test'
+      ? process.env.DB_TEST_URL
+      : process.env.DATABASE_URL,
+  ssl: true,
 };
