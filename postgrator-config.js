6e5d1a0b8b4d9294a8e8cbaec36901960953a1ed
Daniel Sottile <danielsottile@gmail.com>
2021-03-28 11:25:10 -0400

    try this too

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,14 +1,21 @@
 require('dotenv').config();
 
 module.exports = {
-  driver: 'pg',
-  connection: {
-    connectionString:
-      process.env.NODE_ENV === 'test'
-        ? process.env.DB_TEST_URL
-        : process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
+  development: {
+    client: 'pg',
+    connection: {
+      connectionString:
+        process.env.NODE_ENV === 'test'
+          ? process.env.DB_TEST_URL
+          : process.env.DATABASE_URL,
+      ssl: !!process.env.SSL,
+    },
+    production: {
+      client: 'postgresql',
+      connection: {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      },
     },
   },
 };
