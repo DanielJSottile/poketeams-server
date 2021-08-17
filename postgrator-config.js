6d6114f7cc0bbbdedb5c213859b5441aa4c4d131
Daniel Sottile <danielsottile@gmail.com>
2021-08-17 17:02:52 -0400

    saving stuff

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 
 module.exports = {
+  migrationDirectory: 'migrations',
   driver: 'pg',
   connectionString:
     process.env.NODE_ENV === 'test'
       ? process.env.DB_TEST_URL
       : process.env.DATABASE_URL,
-  ssl: !!process.env.SSL,
 };
